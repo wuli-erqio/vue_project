9ec186776084afe65b4f9cd3edad166831734331
wuli-erqio <1766458200@qq.com>
2020-06-10 15:53:59 +0800

    封装请求模块

---
+++ toutiao-m/src/utils/request.js
@@ -0,0 +1,15 @@
+/**
+ * 请求模块
+ */
+import axios from 'axios'
+
+const request = axios.create({
+  // 接口基础路径
+  baseURL: 'http://ttapi.research.itcast.cn/'
+})
+
+// 请求拦截器
+
+// 响应拦截器
+
+export default request
