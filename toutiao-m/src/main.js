00eee4a3bdd188a32498afe0c9c9e1203d7071f9
wuli-erqio <1766458200@qq.com>
2020-06-10 13:13:02 +0800

    导入图标素材

--- toutiao-m/src/main.js
+++ toutiao-m/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 // 加载全局样式
-import  './styles/index'
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
