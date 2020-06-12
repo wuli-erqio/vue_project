f857722d221db3d3c6916eebda18db05966b7f37
wuli-erqio <1766458200@qq.com>
2020-06-12 14:44:30 +0800

    首页-头部导航栏

--- toutiao-m/src/views/layout/home/index.vue
+++ toutiao-m/src/views/layout/home/index.vue
@@ -1,11 +1,23 @@
 <!--  -->
 <template>
-  <div class='home-container'>111</div>
+  <div class='home-container'>
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        slot="title"
+        type="info"
+        size="small"
+        icon="search"
+        class="search-btn"
+        round>搜索</van-button>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+  </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'HomeIndex',
   components: {},
   props: {},
   data () {
@@ -26,4 +38,19 @@ export default {
 }
 </script>
 <style lang='less' scoped>
+.home-container {
+  /deep/.van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+}
 </style>
