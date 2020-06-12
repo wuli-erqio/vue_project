1fc80c4f26e56ab72d51d9b82fdd48a43695bdf4
wuli-erqio <1766458200@qq.com>
2020-06-12 14:31:45 +0800

    个人中心-优化设置Token

--- toutiao-m/src/api/user.js
+++ toutiao-m/src/api/user.js
@@ -2,7 +2,7 @@
  * 用户相关请求模块
  */
 import request from '@/utils/request'
-import store from '@/store'
+// import store from '@/store'
 
 export const login = data => {
   return request({
@@ -28,10 +28,10 @@ export const sendSms = mobile => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
+    url: '/app/v1_0/user'
     // 发送请求头数据
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    // headers: {
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
--- toutiao-m/src/utils/request.js
+++ toutiao-m/src/utils/request.js
@@ -2,6 +2,7 @@
  * 请求模块
  */
 import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
   // 接口基础路径
@@ -9,7 +10,19 @@ const request = axios.create({
 })
 
 // 请求拦截器
-
+request.interceptors.request.use(function (config) {
+  // 请求发起会经过这里
+  // config：本次请求的配置对象
+  const { user } = store.state
+  if (user && user.token) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  // 这里必须要有return 否则请求就出不去，停在这里
+  return config
+}, function (error) {
+  // 如果请求出错了（还没有发出去）会进入这里
+  return Promise.reject(error)
+})
 // 响应拦截器
 
 export default request
