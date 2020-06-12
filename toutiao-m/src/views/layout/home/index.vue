e7cbb980b215bf6d0c31caba43bf98f9499446b5
wuli-erqio <1766458200@qq.com>
2020-06-12 15:11:32 +0800

    首页-处理文章频道列表-样式调整（基础样式）

--- toutiao-m/src/views/layout/home/index.vue
+++ toutiao-m/src/views/layout/home/index.vue
@@ -14,11 +14,14 @@
     <!-- /导航栏 -->
     <!-- 频道列表 -->
     <!-- 通过v-model绑定当前激活标签对应的索引值，默认情况下启用第一个标签 -->
-    <van-tabs v-model="active" animated swipeable>
+    <van-tabs class="channel-tabs" v-model="active" animated swipeable>
       <van-tab title="标签 1">内容 1</van-tab>
       <van-tab title="标签 2">内容 2</van-tab>
       <van-tab title="标签 3">内容 3</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
     </van-tabs>
   </div>
 </template>
@@ -52,6 +55,29 @@ export default {
     font-size: 28px;
     .van-icon {
       font-size: 32px;
+    }
+  }
+  /deep/.channel-tabs {
+    .van-tabs__wrap {
+      height: 82px;
+    }
+    .van-tab {
+      border-right: 1px solid #edeff3;
+      min-width: 200px;
+      font-size: 30px;
+      color: #777777;
+    }
+    .van-tab--active {
+      color: #333333;
+    }
+    .van-tabs__nav {
+      padding-bottom: 0;
+    }
+    .van-tabs__line {
+      bottom: 8px;
+      width: 31px !important;
+      height: 6px;
+      background-color: #3296fa;
     }
   }
 }
