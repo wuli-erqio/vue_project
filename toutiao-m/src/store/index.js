f1e1969732cfd051afe9a12a1063765116079418
wuli-erqio <1766458200@qq.com>
2020-06-12 10:39:52 +0800

    登陆注册-封装本地存储操作模块

--- toutiao-m/src/store/index.js
+++ toutiao-m/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { getItem, setItem } from '@/utils/storage'
 Vue.use(Vuex)
 
 const TOKEN_KEY = 'TOUTIAO_USER'
@@ -8,14 +8,16 @@ const TOKEN_KEY = 'TOUTIAO_USER'
 export default new Vuex.Store({
   state: {
     // 一个对象，用于存储当前登录用户信息，token等数据
-    user: JSON.parse(window.localStorage.getItem(TOKEN_KEY))
+    user: getItem(TOKEN_KEY)
+    // user: JSON.parse(window.localStor.getItem(TOKEN_KEY))
     // user: null
   },
   mutations: {
     setUser (state, data) {
       state.user = data
       // 为了防止刷新丢失，需要把数据备份到本地存储
-      window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      // window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      setItem(TOKEN_KEY, state.user)
     }
   },
   actions: {
