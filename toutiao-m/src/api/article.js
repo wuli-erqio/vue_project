0dd733a8ede3aead7ec3bc798403971cac02bcf3
wuli-erqio <1766458200@qq.com>
2020-06-15 11:48:04 +0800

    首页-展示文章列表-处理响应结果

---
+++ toutiao-m/src/api/article.js
@@ -0,0 +1,15 @@
+/**
+ * 文章相关请求模块
+ */
+import request from '@/utils/request'
+
+/**
+ * 请求获取文章列表数据
+ */
+export const getArticles = params => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_1/articles',
+    params
+  })
+}
--- toutiao-m/src/views/layout/home/components/article-list.vue
+++ toutiao-m/src/views/layout/home/components/article-list.vue
@@ -7,12 +7,13 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell v-for="(article, index) in list" :key="index" :title="article.title" />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getArticles } from '@/api/article'
 export default {
   name: 'ArticleList',
   components: {},
@@ -26,30 +27,42 @@ export default {
     return {
       list: [], // 存储列表数据的数组
       loading: false, // 控制加载中loading状态
-      finished: false // 控制数据加载结束状态
+      finished: false, // 控制数据加载结束状态
+      timestamp: null
     }
   },
   computed: {},
   watch: {},
   methods: {
-    onLoad () {
-      // 1.请求获取数组
-      setTimeout(() => {
-        // 2.把请求结果数据添加到list数组中
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
+    async onLoad () {
+      try {
+        // 1.请求获取数组
+        const { data } = await getArticles({
+          channel_id: this.channel.id, // 频道id
+          // 简单理解就是请求的数据页码
+          // 请求第一页数据：当前最新时间戳
+          // 用于请求之后的时间戳会在当前请求结果中返回给你
+          timestamp: this.timestamp || Date.now(),
+          // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+          with_top: 1
+        })
 
+        // 2.把请求结果数据添加到list数组中
+        const { results } = data.data
+        // 数组的展开操作符，他会把数组元素一个一个拿出来
+        this.list.push(...results)
         // 3.本次数据加载结束之后要把加载状态设置为结束
         // loading 关闭以后才能触发下一次的加载更多
         this.loading = false
-
         // 4.判断数据是否全部加载完成
-        if (this.list.length >= 40) {
-          // finished 设置为true, 之后不在触发加载更多
+        if (results.length) {
+          // 更新获取下一页数据的时间戳
+          this.timestamp = data.data.pre_timestamp
+        } else {
+          // 没有数据了，finished 设置为true, 之后不在触发加载更多
           this.finished = true
         }
-      }, 1000)
+      } catch (err) {}
     }
   },
   created () {}
