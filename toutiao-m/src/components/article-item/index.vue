f592715b39cf3ed3d5dc304cfb2192073a4936fd
wuli-erqio <1766458200@qq.com>
2020-06-15 15:06:47 +0800

    首页-文章列表项-样式调整

--- toutiao-m/src/components/article-item/index.vue
+++ toutiao-m/src/components/article-item/index.vue
@@ -1,19 +1,13 @@
 <template>
-  <van-cell
-    class="article-item">
-    <div slot="title" class="title">{{ article.title }}</div>
+  <van-cell class="article-item">
+    <div slot="title" class="title van-multi-ellipsis--l2">{{ article.title }}</div>
     <div slot="label" class="">
       <div v-if="article.cover.type === 3" class="cover-wrap">
         <div class="cover-item" v-for="(img, index) in article.cover.images" :key="index">
-          <van-image
-          width="100"
-          height="100"
-          :src="img" />
+          <van-image class="cover-item-img" fit="cover" :src="img" />
         </div>
-        <div class="cover-item"></div>
-        <div class="cover-item"></div>
       </div>
-      <div>
+      <div class="label-info-wrap">
         <span>{{ article.aut_name }}</span>
         <span>{{ article.comm_count }}评论</span>
         <span>{{ article.pubdate }}</span>
@@ -22,8 +16,8 @@
     <van-image
       v-if="article.cover.type === 1"
       slot="default"
-      width="100"
-      height="100"
+      class="right-cover"
+      fit="cover"
       :src="article.cover.images[0]" />
   </van-cell>
 </template>
@@ -49,4 +43,41 @@ export default {
 }
 </script>
 <style lang='less' scoped>
+.article-item {
+  .title {
+    font-style: 32px;
+    color: #3a3a3a;
+  }
+
+  .van-cell__value {
+    flex: unset;
+    width: 232px;
+    height: 146px;
+    padding-left: 25px;
+  }
+  .right-cover {
+    width: 232px;
+    height: 146px;
+  }
+  .label-info-wrap span {
+    height: 22px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+  .cover-wrap {
+    display: flex;
+    padding: 30 0;
+    .cover-item {
+      flex: 1;
+      height: 146px;
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-item-img {
+        width: 100%;
+        height: 146px;
+      }
+    }
+  }
+}
 </style>
