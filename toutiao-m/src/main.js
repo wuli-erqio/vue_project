9a2346193fc1a885f78575c26109930c8e536fb7
wuli-erqio <1766458200@qq.com>
2020-06-15 15:28:31 +0800

    首页-文章列表项-处理相对时间（初始化DayJS配置）

--- toutiao-m/src/main.js
+++ toutiao-m/src/main.js
@@ -13,6 +13,9 @@ import './styles/index.less'
 // 动态设置基准值
 import 'amfe-flexible'
 
+// 加载dayjs初始化配置
+import './utils/dayjs'
+
 // 注册使用后vant组件
 Vue.use(Vant)
 
---
+++ toutiao-m/src/utils/dayjs.js
@@ -0,0 +1,8 @@
+import dayjs from 'dayjs'
+// dayjs 默认语言是英文，在这配置成中文
+
+// 加载中文语言包
+import 'dayjs/locale/zh-cn'
+
+// 全局使用
+dayjs.locale('zh-cn')
