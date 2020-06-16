1ab52276f9ed5d506030e2c7788b0103b2920d70
wuli-erqio <1766458200@qq.com>
2020-06-16 10:07:03 +0800

    频道编辑-样式调整

--- toutiao-m/src/views/layout/home/components/channel-edit.vue
+++ toutiao-m/src/views/layout/home/components/channel-edit.vue
@@ -2,24 +2,28 @@
 <template>
   <div class='channel-edit'>
     <van-cell :border="false">
-      <div slot="title">我的频道</div>
-      <van-button type="danger" round plain size="mini">编辑</van-button>
+      <div slot="title" class="title-text">我的频道</div>
+      <van-button type="danger" class="edit-btn" round plain size="mini">编辑</van-button>
     </van-cell>
-    <van-grid :gutter="10">
+    <van-grid class="my-grid" :gutter="10">
       <van-grid-item
+        class="grid-item"
         v-for="value in 8"
         :key="value"
+        icon="clear"
         text="文字" />
     </van-grid>
     <!-- 频道推荐 -->
     <van-cell :border="false">
-      <div slot="title">频道推荐</div>
+      <div slot="title" class="title-text">频道推荐</div>
     </van-cell>
-    <van-grid :gutter="10">
+    <van-grid class="recommend-grid" :gutter="10">
       <van-grid-item
+        class="grid-item"
         v-for="value in 8"
         :key="value"
-        text="文字" />
+        icon="plus"
+        text="文字文字" />
     </van-grid>
     <!-- /频道推荐 -->
   </div>
@@ -42,5 +46,55 @@ export default {
 <style lang='less' scoped>
 .channel-edit {
   padding: 85px 0;
+  .title-text {
+    font-size: 32px;
+    color: #333;
+  }
+  .edit-btn {
+    width: 104px;
+    height: 48px;
+    font-size: 26px;
+    color: #f85959;
+    border: 1px solid #f85959;
+  }
+  /deep/.grid-item {
+    width: 160px;
+    height: 86px;
+    .van-grid-item__content {
+      background-color: #f4f5f6;
+      white-space: nowrap;
+      .van-grid-item__text {
+        font-size: 28px;
+        color: #222;
+        margin-top: 0;
+      }
+    }
+  }
+  /deep/ .my-grid {
+    .grid-item {
+      .van-icon-clear {
+        position: absolute;
+        right: -10px;
+        top: -10px;
+        font-size: 30px;
+        color: #ccc;
+        z-index: 2;
+      }
+    }
+  }
+  /deep/.recommend-grid {
+    .grid-item {
+      .van-grid-item__content {
+        flex-direction: row;
+        .van-icon-plus {
+          font-size: 28px;
+          margin-right: 6px;
+        }
+        .van-grid-item__text {
+          margin-top: 0;
+        }
+      }
+    }
+  }
 }
 </style>
