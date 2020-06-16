126807192c74560bdff7935d6b9729516af5461b
wuli-erqio <1766458200@qq.com>
2020-06-16 14:50:31 +0800

    文章搜索-页面布局-搜索框

--- toutiao-m/src/views/search/index.vue
+++ toutiao-m/src/views/search/index.vue
@@ -1,6 +1,21 @@
 <!--  -->
 <template>
-  <div class='search-container'>11</div>
+  <div class='search-container'>
+    <!-- 顶部搜索栏 -->
+    <!--  在 van-search 外层增加 form 标签，
+          且 action 不为空，即可在 iOS 输入法中显示搜索按钮 -->
+    <form action="/">
+      <van-search
+        v-model="searchText"
+        show-action
+        placeholder="请输入搜索关键词"
+        background="#3296fa"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
+    <!-- /顶部搜索栏 -->
+  </div>
 </template>
 
 <script>
@@ -9,21 +24,27 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      searchText: ''
+    }
   },
   computed: {},
   watch: {},
-  methods: {},
-  created () {},
-  mounted () {},
-  beforeCreate () {},
-  beforeMount () {},
-  beforeUpdate () {},
-  updated () {},
-  beforeDestroy () {},
-  destroyed () {},
-  activated () {}
+  methods: {
+    onSearch (val) {
+      this.$toast(val)
+    },
+    onCancel () {
+      this.$router.back()
+    }
+  },
+  created () {}
 }
 </script>
 <style lang='less' scoped>
+.search-container {
+  .van-search__action {
+    color: #fff;
+  }
+}
 </style>
