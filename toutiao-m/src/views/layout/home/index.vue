f6e8f9ccf653e4c75599bbf30c8c224255ccadd1
wuli-erqio <1766458200@qq.com>
2020-06-16 14:30:09 +0800

    频道编辑-正确的展示频道列表

--- toutiao-m/src/views/layout/home/index.vue
+++ toutiao-m/src/views/layout/home/index.vue
@@ -46,6 +46,8 @@
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list'
 import ChannelEdit from './components/channel-edit'
+import { mapState } from 'vuex'
+import { getItem } from '@/utils/storage'
 export default {
   name: 'HomeIndex',
   components: {
@@ -60,13 +62,31 @@ export default {
       isChannelEditShow: false // 控制编辑频道弹出层的显示与隐藏
     }
   },
-  computed: {},
+  computed: {
+    ...mapState(['user'])
+  },
   watch: {},
   methods: {
     async loadChannels () {
       try {
-        const { data } = await getUserChannels()
-        this.channels = data.data.channels
+        let channels = []
+        if (this.user) {
+          // 已登录，请求获取用户列表
+          const { data } = await getUserChannels()
+          channels = data.data.channels
+        } else {
+          // 未登录，判断是否有本地的频道列表数据
+          const localChannels = getItem('TOUTIAO_CHANNELS')
+          if (localChannels) {
+            // 有，拿来使用
+            channels = localChannels
+          } else {
+            // 没有，请求获取默认频道列表
+            const { data } = await getUserChannels()
+            channels = data.data.channels
+          }
+        }
+        this.channels = channels
       } catch (err) {
         this.$toast('获取频道列表数据失败')
       }
