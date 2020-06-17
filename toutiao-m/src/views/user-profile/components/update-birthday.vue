434724305d0ce613fcebf3ec8f058b0b6ac45d93
wuli-erqio <1766458200@qq.com>
2020-06-17 14:55:56 +0800

    编辑用户资料-修改生日-处理完成

--- toutiao-m/src/views/user-profile/components/update-birthday.vue
+++ toutiao-m/src/views/user-profile/components/update-birthday.vue
@@ -1,19 +1,73 @@
 <!--  -->
 <template>
-  <div class='update-birthday'>生日</div>
+  <div class='update-birthday'>
+    <!--
+      currentDate 双向绑定了日期选择器
+        设置日期选择器的默认值
+        同步日期选择器选择日期
+      min-date 可选的最小日期
+      max-date 可选的最大日期
+    -->
+    <van-datetime-picker
+      v-model="currentDate"
+      type="date"
+      :min-date="minDate"
+      :max-date="maxDate"
+      @cancel="$emit('close')"
+      @confirm="onConfirm"
+    />
+  </div>
 </template>
 
 <script>
+import { updateUserProfile } from '@/api/user'
+import dayjs from 'dayjs'
+
 export default {
   name: 'UpdateBirthday',
   components: {},
-  props: {},
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
   data () {
-    return {}
+    return {
+      minDate: new Date(1970, 0, 1),
+      maxDate: new Date(),
+      currentDate: new Date(this.value)
+    }
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    async onConfirm (value, index) {
+      this.$toast.loading({
+        message: '保存中...',
+        forbidClick: true, // 禁止背景点击
+        duration: 0 // 持续展示
+      })
+      try {
+        const currentDate = dayjs(this.currentDate).format('YYYY-MM-DD')
+        const { data } = await updateUserProfile({
+          birthday: currentDate
+        })
+        // 更新视图
+        this.$emit('input', currentDate)
+        // 关闭弹层
+        this.$emit('close')
+        // 提示成功
+        this.$toast.success('更新成功')
+        console.log(data)
+      } catch (error) {
+        this.$toast.fail('更新失败')
+      }
+    },
+    onPickerChange (picker, value, index) {
+      this.currentDate = index
+    }
+  },
   created () {}
 }
 </script>
