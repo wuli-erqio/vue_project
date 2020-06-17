c6efb4f6489ddf1ec27f6774a18426d7ab77ed28
wuli-erqio <1766458200@qq.com>
2020-06-17 17:12:03 +0800

    编辑用户资料-修改头像-图片裁剪（配置说明）

--- toutiao-m/src/views/user-profile/components/update-photo.vue
+++ toutiao-m/src/views/user-profile/components/update-photo.vue
@@ -31,16 +31,28 @@ export default {
   mounted () {
     const image = this.$refs.img
     const cropper = new Cropper(image, {
-      aspectRatio: 16 / 9,
-      crop (event) {
-        console.log(event.detail.x)
-        console.log(event.detail.y)
-        console.log(event.detail.width)
-        console.log(event.detail.height)
-        console.log(event.detail.rotate)
-        console.log(event.detail.scaleX)
-        console.log(event.detail.scaleY)
-      }
+      // 定义cropper的查看模式。
+      // 如果您将viewMode设置为0，那么裁切框可以扩展到画布之外，
+      // 而1、2或3的值将限制裁切框的大小到画布的大小。
+      // 视图模式为2或3会额外地将画布限制在容器中。
+      // 注意，如果画布和容器的比例相同，那么2和3之间没有区别
+      viewMode: 1,
+      // 'crop': 鼠标创建一个裁剪框
+      // 'move': 只拖动图片
+      // 'none': 只拖动裁剪框
+      dragMode: 'move',
+      // 截图比例，默认19/9
+      aspectRatio: 1,
+      // 自动截取比例
+      autoCropArea: 1,
+      // 画布移动
+      cropBoxMovable: false,
+      // 截图区域的缩放
+      cropBoxResizable: false,
+      // 背景
+      background: false,
+      // 画布是否可以移动，默认可以
+      movable: true
     })
     console.log(cropper)
   }
