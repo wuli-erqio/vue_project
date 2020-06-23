1a3540f98047ddaa9a0ba01ef8979b9c07580d8c
wuli-erqio <1766458200@qq.com>
2020-06-23 10:11:58 +0800

    文章详情-页面布局

--- toutiao-m/src/views/article/index.vue
+++ toutiao-m/src/views/article/index.vue
@@ -1,5 +1,70 @@
 <template>
-  <div class='article-container'></div>
+  <div class='article-container'>
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" left-arrow title="力创"></van-nav-bar>
+    <div class="main-warp">
+      <!-- 加载中 -->
+      <div class="loading-warp">
+        <van-loading color="#3296fa" vertical>
+          加载中
+        </van-loading>
+      </div>
+      <!-- 加载完成-文章详情 -->
+      <div class="article-detail">
+        <!-- 文章标题 -->
+        <h1 class="article-title">这是文章标题</h1>
+        <!-- 用户信息 -->
+        <van-cell class="user-info" center :border="false">
+          <van-image
+            class="avatar"
+            slot="icon"
+            round
+            fit="cover"
+            src="https://img.yzcdn.cn/vant/cat.jpeg"
+          />
+          <div slot="title" class="user-name">力创头条</div>
+          <div slot="label" class="publish-date">14小时前</div>
+          <van-button
+            class="follow-btn"
+            type="info"></van-button>
+        </van-cell>
+      </div>
+      <!-- 加载失败: 404 -->
+      <div class="error-wrap">
+        <van-icon name="failure"></van-icon>
+        <p class="text">该资源不存在或已删除!</p>
+      </div>
+      <!-- 加载失败:其他未知错误(例如:网络原因或服务器异常) -->
+      <div class="error-wrap">
+        <van-icon name="failure"></van-icon>
+        <p class="text">内容加载失败!</p>
+        <van-button class="retry-btn">点击重试</van-button>
+      </div>
+    </div>
+    <!-- 底部区域 -->
+    <div class="article-bottom">
+      <van-button
+        class="comment-btn"
+        type="default"
+        round
+        size="small"
+      >写评论</van-button>
+      <van-icon
+        name="comment-o"
+        info="123"
+        color="#777"
+      />
+      <van-icon
+        color="#777"
+        name="star-o"
+      />
+      <van-icon
+        color="#777"
+        name="good-job-o"
+      />
+      <van-icon name="share" color="#777777"></van-icon>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -23,4 +88,117 @@ export default {
 }
 </script>
 <style lang='less' scoped>
+.article-container {
+  .main-wrap {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 92px;
+    bottom: 88px;
+    overflow-y: scroll;
+    background-color: #fff;
+  }
+  .article-detail {
+    .article-title {
+      font-size: 40px;
+      padding: 50px 32px;
+      margin: 0;
+      color: #3a3a3a;
+    }
+
+    .user-info {
+      padding: 0 32px;
+      .avatar {
+        width: 70px;
+        height: 70px;
+        margin-right: 17px;
+      }
+      .van-cell__label {
+        margin-top: 0;
+      }
+      .user-name {
+        font-size: 24px;
+        color: #3a3a3a;
+      }
+      .publish-date {
+        font-size: 23px;
+        color: #b7b7b7;
+      }
+      .follow-btn {
+        width: 170px;
+        height: 58px;
+      }
+    }
+
+    .article-content {
+      padding: 55px 32px;
+      /deep/ p {
+        text-align: justify;
+      }
+    }
+  }
+
+  .loading-wrap {
+    padding: 200px 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+  }
+
+  .error-wrap {
+    padding: 200px 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    .van-icon {
+      font-size: 122px;
+      color: #b4b4b4;
+    }
+    .text {
+      font-size: 30px;
+      color: #666666;
+      margin: 33px 0 46px;
+    }
+    .retry-btn {
+      width: 280px;
+      height: 70px;
+      line-height: 70px;
+      border: 1px solid #c3c3c3;
+      font-size: 30px;
+      color: #666666;
+    }
+  }
+
+  .article-bottom {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    box-sizing: border-box;
+    height: 88px;
+    border-top: 1px solid #d8d8d8;
+    background-color: #fff;
+    .comment-btn {
+      width: 282px;
+      height: 46px;
+      border: 2px solid #eeeeee;
+      font-size: 30px;
+      line-height: 46px;
+      color: #a7a7a7;
+    }
+    .van-icon {
+      font-size: 40px;
+      .van-info {
+        font-size: 16px;
+        background-color: #e22829;
+      }
+    }
+  }
+}
 </style>
