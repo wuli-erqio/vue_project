6bbd132239d536fc0342140b8c6e6fdb3967ba84
wuli-erqio <1766458200@qq.com>
2020-07-17 09:40:49 +0800

    流光倒影

--- kanban/src/views/CSS/button.vue
+++ kanban/src/views/CSS/button.vue
@@ -12,7 +12,15 @@
         <div class="col">
           <span>流光按钮</span>
         </div>
-        <div class="col"></div>
+        <div class="col">
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            流光倒影
+          </div>
+        </div>
         <div class="col"></div>
       </div>
       <div class="row">
@@ -198,6 +206,99 @@ export default {
             background-position: -400% 0;
           }
         }
+      }
+      .col:nth-child(3) {
+        div {
+          position: relative;
+          padding: 25px 30px;
+          color: #03a9f4;
+          font-size: 24px;
+          text-decoration: none;
+          transition: .5s;
+          overflow: hidden;
+          letter-spacing: 4px;
+          -webkit-box-reflect: below 1px linear-gradient(transparent, #0005);
+          filter: hue-rotate(290deg);
+        }
+        div:hover {
+          background: #03e9f4;
+          color: #050801;
+          box-shadow: 0 0 5px #03e9f4,
+                      0 0 25px #03e9f4,
+                      0 0 50px #03e9f4,
+                      0 0 200px #03e9f4;
+        }
+        span {
+            position: absolute;
+            display: block;
+          }
+          span:nth-child(1) {
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 2px;
+            background:linear-gradient(90deg, transparent,#03e9f4);
+            animation: animation1 1s linear infinite;
+          }
+          @keyframes animation1 {
+            0% {
+              left:  -100%;
+            }
+            50%,100% {
+              left: 100%;
+            }
+          }
+          span:nth-child(2) {
+            top: -100%;
+            right: 0;
+            width: 2px;
+            height: 100%;
+            background: linear-gradient(180deg, transparent,#03e9f4);
+            animation: animation2 1s linear infinite;
+            animation-delay: 0.25s;
+          }
+          @keyframes animation2 {
+            0% {
+              top:  -100%;
+            }
+            50%,100% {
+              top: 100%;
+            }
+          }
+          span:nth-child(3) {
+            bottom: 0;
+            right: -100%;
+            width: 100%;
+            height: 2px;
+            background: linear-gradient(270deg, transparent,#03e9f4);
+            animation: animation3 1s linear infinite;
+            animation-delay: 0.5s;
+          }
+          @keyframes animation3 {
+            0% {
+              right:  -100%;
+            }
+            50%,100% {
+              right: 100%;
+            }
+          }
+          span:nth-child(4) {
+            bottom: -100%;
+            left: 0;
+            width: 2px;
+            height: 100%;
+            background: linear-gradient(360deg, transparent,#03e9f4);
+            animation: animation4 1s linear infinite;
+            animation-delay: 0.75s;
+          }
+          @keyframes animation4 {
+            0% {
+              bottom:  -100%;
+            }
+            50%,100% {
+              bottom: 100%;
+            }
+          }
       }
     }
     .row:nth-child(2) {
